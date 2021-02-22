739d9b08d2f761efc5edbe5e5c3ec6f0ed716943
Nick Hempsey <nick@elementmarketingcompany.com>
2021-02-21 20:02:41 -0500

    Page 38
    Calling it a night, sleepy.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,37 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // TODO: Store response from guest
+
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
         }
     }
 }
---
+++ Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
